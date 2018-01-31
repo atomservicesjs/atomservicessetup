dc0249fb207e09c3e1eadfdf04c41412a479bb88
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-01-31 16:18:32 +0700

    RELEASE: v.0.1.0-alpha.11

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ import { PathResolver, ValueLoader } from "./util";
 export { ApplicationSetupLoader } from "./application.setup.loader";
 export { ModularSetupLoader } from "./modular.setup.loader";
 export { CompositionSetupLoader } from "./composition.setup.loader";
+export { SetupFile } from "./setupfile";
 export const Util = {
   PathResolver,
   ValueLoader
